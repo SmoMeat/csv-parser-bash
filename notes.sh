72a1b3d773b18099d9ef943f5c709febc8a80038
admin <user@Test-shell.myguest.virtualbox.org>
2024-04-28 17:18:34 -0400

    add ponderation reading

--- notes.sh
+++ notes.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
 
-HEADER=$(head -n 1 liste.csv)
-REGEX="([A-Za-z0-9]+)\(([0-9.]+)\)"
+# Separe la 1e ligne aux virgules
+IFS=',' read -r -a pairs <<< $(head -n 1 $1)
 
-definir_ponderation() {
-    local HEADER="$1"
-    while [[ $HEADER =~ $REGEX ]]; do
-        local VARIABLE="${BAS_REMATCH[1]}"
-        local VALEUR="${BAS_REMATCH[2]}"
-        echo "$VARIABLE=$VALEUR"
-        declare "$VARIABLE=$VALEUR"
-        HEADER="${HEADER/${BASH_REMATCH[0]}/}"
-    done
-}
-#definir_ponderation "$HEADER"
+# Assigne la ponderation (val) a chaque examen (var)
+for pair in "${pairs[@]:2}"; do
+    var_name=$(echo "$pair" | cut -d'(' -f1)
+    var_value=$(echo "$pair" | cut -d'(' -f2 | cut -d')' -f1)
+    declare "ponderation_$var_name=$var_value"
+done
 
-# Définition des pondérations
-# Faire en sorte que ca se calcule tout seul
-POND_TP1=7.5
-POND_INTRA=3
-POND_TP2=3
-POND_TP3=3
-POND_FINAL=3
-POND_TP4=3
-
-echo $POND_TP1
-echo $POND_TP2
-echo $POND_FINAL
+echo "Code permanent,Nom,Note ponderee" > notes.csv
 
 while IFS=',' read -r CODE NOM TP1 INTRA TP2 TP3 FINAL TP4; do
     if [[ $CODE != "Code permanent" ]]; then
-        NOTE_PONDEREE=$(echo "scale=2; ($TP1 * $POND_TP1 + $INTRA * $POND_INTRA + $TP2 * $POND_TP2 + $TP3 * $POND_TP3 + $FINAL * $POND_FINAL + $TP4 * $POND_TP4) / ($POND_TP1 + $POND_INTRA + $POND_TP2 + $POND_TP3 + $POND_FINAL + $POND_TP4)" | bc)
+        NOTE_PONDEREE=$(echo "scale=2; ($TP1 * $ponderation_TP1 + $INTRA * $ponderation_Intra + $TP2 * $ponderation_TP2 + $TP3 * $ponderation_TP3 + $FINAL * $ponderation_Final + $TP4 * $ponderation_TP4) / ($ponderation_TP1 + $ponderation_Intra + $ponderation_TP2 + $ponderation_TP3 + $ponderation_Final + $ponderation_TP4)" | bc)
 	echo "$CODE,$NOM,$NOTE_PONDEREE"
     fi
-done < "$1" > notes.csv
+done < "$1" >> notes.csv
+
+echo "Les notes sont accessibles dans le fichier notes.csv"
